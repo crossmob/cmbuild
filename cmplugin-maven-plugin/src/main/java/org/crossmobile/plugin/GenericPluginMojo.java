ef5a5073d29388eb1f43bbc9cb402157610a8a7e
Panayotis Katsaloulis <panayotis@panayotis.com>
2020-08-05 04:20:45 +0300

    added missing header

--- cmplugin-maven-plugin/src/main/java/org/crossmobile/plugin/GenericPluginMojo.java
+++ cmplugin-maven-plugin/src/main/java/org/crossmobile/plugin/GenericPluginMojo.java
@@ -1,3 +1,9 @@
+/*
+ * (c) 2020 by Panayotis Katsaloulis
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
 package org.crossmobile.plugin;
 
 import org.apache.maven.plugin.MojoExecutionException;
